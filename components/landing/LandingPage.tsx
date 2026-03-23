9068cf4de123f81a736e4296949cd0d551ca58a5
Ignazio Maria Castrignano <104200544+Irio5@users.noreply.github.com>
2026-03-23 22:25:20 +0100

    Change background size from 120% to 80%

--- components/landing/LandingPage.tsx
+++ components/landing/LandingPage.tsx
@@ -152,7 +152,7 @@ export default function LandingPage() {
           className="absolute inset-0 hidden md:block"
           style={{
             backgroundImage: `url("/images/Brera_House.webp")`,
-            backgroundSize: "120%",
+            backgroundSize: "80%",
             backgroundPosition: "center 0%",
           }}
         />
